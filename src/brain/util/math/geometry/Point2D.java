27dc6c8763887b2ea24bc656eee0ffdfefa41e35
rafael-dourado <cpd.h6@shibata.com.br>
2017-06-29 16:33:39 -0300

    Update Point2D.java
    correção do equals

--- src/brain/util/math/geometry/Point2D.java
+++ src/brain/util/math/geometry/Point2D.java
@@ -4,8 +4,9 @@ public class Point2D {
 	private double x;
 	private double y;
 	public final static Point2D ORIGIN = new Point2D(0.0, 0.0);
+	private final static double EPSILON = 0.00000001;
 
-	Point2D(double x, double y) {
+	public Point2D(double x, double y) {
 		this.x = x;
 		this.y = y;
 	}
@@ -25,4 +26,24 @@ public class Point2D {
 		return Math.sqrt(w + h);
 	}
 
+	public boolean equals(Point2D p) {
+		if (p != null) {
+			if (!Double.isFinite(p.getX()) && !Double.isFinite(p.getY()) && !Double.isFinite(getX())
+					&& !Double.isFinite(getY()))
+				return Math.abs(p.getX() - getX()) <= EPSILON && Math.abs(p.getY() - getY()) <= EPSILON;
+			else
+				return p.getX() == this.getX() && p.getY() == this.getY();
+		}
+
+		return false;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (o != null && o.getClass() == this.getClass()) {
+			Point2D other = (Point2D) o;
+			return this.equals(other);
+		}
+		return false;
+	}
 }
