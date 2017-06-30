1750606a2b8018c091204165e59911474fb811f9
rafael-dourado <cpd.h6@shibata.com.br>
2017-06-29 23:05:59 -0300

    Update Polygon2D.java

--- src/brain/util/math/geometry/Polygon2D.java
+++ src/brain/util/math/geometry/Polygon2D.java
@@ -1,9 +1,13 @@
-package brain.util.math.geometry;
+package brain.util.math;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
+import brain.util.math.Point2D;
 public class Polygon2D {
+
 
 	private List<Point2D> vertexes;
 
@@ -21,4 +25,45 @@ public class Polygon2D {
 		}
 	}
 
+
+	public boolean isValid(){
+		return false;
+	} 
+
+	public boolean intersects(Polygon2D other){
+		for(Point2D p : other.getVertexes()){
+			if(this.hasInside(p))
+				return true;
+		}
+		return false;
+	}
+
+	
+	
+	public boolean hasInside(Point2D point){
+		int count = 0;
+		Set<Point2D> vertexesIntAux = new HashSet<Point2D>();
+		Line2D auxLine = new Line2D(new Point2D(0.0,point.getY()), point);
+		for(int i = 0; i < vertexes.size(); i++){
+			Point2D a = new Point2D(vertexes.get(i));
+			Point2D b;
+			
+			if(i != vertexes.size()-1)
+				b = new Point2D(vertexes.get(i+1));
+			else
+				b = new Point2D(vertexes.get(0));
+			
+			if(auxLine.contains(a)){
+				vertexesIntAux.add(a);
+			}
+			
+			Line2D edge = new Line2D(a,b);
+			if(edge.getIntersctPointIn(auxLine) != null)
+				count++;	
+		}
+		
+		return (count - vertexesIntAux.size()) % 2 == 1;
+	}
+
 }
+
