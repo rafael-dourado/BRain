39f15c34b3d69e5dd94a4d9dbbeb495236ac9ad5
rafael-dourado <cpd.h6@shibata.com.br>
2017-07-01 11:58:29 -0300

    Update Polygon2D.java

--- src/brain/util/math/geometry/Polygon2D.java
+++ src/brain/util/math/geometry/Polygon2D.java
@@ -1,13 +1,11 @@
-package brain.util.math;
+package brain.util.math.geometry;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
-import brain.util.math.Point2D;
+import brain.util.Util;
+
 public class Polygon2D {
-
 
 	private List<Point2D> vertexes;
 
@@ -25,45 +23,77 @@ public class Polygon2D {
 		}
 	}
 
-
-	public boolean isValid(){
+	public boolean isValid() {
 		return false;
-	} 
+	}
 
-	public boolean intersects(Polygon2D other){
-		for(Point2D p : other.getVertexes()){
-			if(this.hasInside(p))
+	public boolean intersects(Polygon2D other) {
+		for (Point2D p : other.getVertexes()) {
+			if (this.hasInside(p))
 				return true;
 		}
 		return false;
 	}
 
-	
-	
-	public boolean hasInside(Point2D point){
-		int count = 0;
-		Set<Point2D> vertexesIntAux = new HashSet<Point2D>();
-		Line2D auxLine = new Line2D(new Point2D(0.0,point.getY()), point);
-		for(int i = 0; i < vertexes.size(); i++){
-			Point2D a = new Point2D(vertexes.get(i));
-			Point2D b;
-			
-			if(i != vertexes.size()-1)
-				b = new Point2D(vertexes.get(i+1));
-			else
-				b = new Point2D(vertexes.get(0));
-			
-			if(auxLine.contains(a)){
-				vertexesIntAux.add(a);
-			}
-			
-			Line2D edge = new Line2D(a,b);
-			if(edge.getIntersctPointIn(auxLine) != null)
-				count++;	
+	public double getGreaterX() {
+		double greaterX = Double.NEGATIVE_INFINITY;
+		for (Point2D vertex : vertexes) {
+			if (vertex.getX() > greaterX)
+				greaterX = vertex.getX();
 		}
-		
-		return (count - vertexesIntAux.size()) % 2 == 1;
+
+		return greaterX;
+	}
+
+	public double getGreaterY() {
+		double greaterY = Double.NEGATIVE_INFINITY;
+		for (Point2D vertex : vertexes) {
+			if (vertex.getY() > greaterY)
+				greaterY = vertex.getX();
+		}
+
+		return greaterY;
+	}
+
+	public double getLeastX() {
+		double leastX = Double.POSITIVE_INFINITY;
+		for (Point2D vertex : vertexes) {
+			if (vertex.getX() < leastX)
+				leastX = vertex.getX();
+		}
+
+		return leastX;
+	}
+
+	public double getLeastY() {
+		double leastY = Double.POSITIVE_INFINITY;
+		for (Point2D vertex : vertexes) {
+			if (vertex.getX() < leastY)
+				leastY = vertex.getX();
+		}
+
+		return leastY;
+	}
+
+	public boolean hasInside(Point2D point) {
+		if (Util.compare(point.getX(), getGreaterX()) > 0 || Util.compare(point.getX(), getLeastX()) < 0)
+			return false;
+
+		if (Util.compare(point.getY(), getGreaterY()) > 0 || Util.compare(point.getY(), getLeastY()) < 0)
+			return false;
+		// cria uma linha paralela ao eixo x, onde y é constante e vai até x =
+		// point.get();
+		Line2D auxLine = new Line2D(new Point2D(0.0, point.getY()), point);
+
+		for (int i = 0; i < vertexes.size(); i++) {
+			Point2D a = new Point2D(vertexes.get(i));
+			if (i != vertexes.size() - 1) {
+				Point2D b = new Point2D(vertexes.get(i + 1));
+			} else {
+				Point2D b = new Point2D(vertexes.get(0));
+			}
+		}
+		return true;
 	}
 
 }
-
