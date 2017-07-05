c865c0d2c5b37eaa587cada8371ff9631225e833
rafael-dourado <cpd.h6@shibata.com.br>
2017-07-05 14:23:52 -0300

    Create CartesianPlane.java

---
+++ src/brain/util/math/geometry/CartesianPlane.java
@@ -0,0 +1,56 @@
+package brain.util.math.geometry;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CartesianPlane {
+	enum Quadrant {
+		FIRST_QUADRANT, SECOND_QUADRANT, THIRD_QUANDRANT, FOURTH_QUADRANT
+	}
+
+	private List<Point2D> points;
+	private List<Line2D> lines;
+	private List<Polygon2D> polygons;
+
+	public CartesianPlane() {
+		this.points = new ArrayList<>();
+		this.lines = new ArrayList<>();
+		this.polygons = new ArrayList<>();
+	}
+
+	public List<Point2D> getPoints() {
+		return this.points;
+	}
+
+	public List<Line2D> getLines() {
+		return this.lines;
+	}
+
+	public List<Polygon2D> getPolygons() {
+		return this.polygons;
+	}
+
+	public boolean add(Point2D point) {
+		return points.add(point);
+	}
+
+	public boolean add(Line2D line) {
+		return lines.add(line);
+	}
+
+	public boolean add(Polygon2D polygon) {
+		return polygons.add(polygon);
+	}
+
+	public boolean remove(Point2D point) {
+		return points.remove(point);
+	}
+
+	public boolean remove(Line2D line) {
+		return points.remove(line);
+	}
+
+	public boolean remove(Polygon2D polygon) {
+		return polygons.remove(polygon);
+	}
+}
